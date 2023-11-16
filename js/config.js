af137e597f0bba529e20216c0306492020d65cb1
super181818 <131360198+super181818@users.noreply.github.com>
2023-11-16 16:56:15 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 /*-------config start-------*/
-var _wip = '111.230.237.241';  
+var _wip = 'yylx.panguh5.com';  
 var _port = '1714';   
 /*---------config end---------*/
 
@@ -8,6 +8,5 @@ var ResourceUrl = '/';
 var Suffix = '.html';
 var _isTx = 1;
 var _wport = '10000' ;
-
-
-/*document.write('<script type="text/javascript" src="http://' + _wip + ':1999/dyoss.js?v=' + _tt + '"> <\/script>');*/
+BaseUrl = 'http://yyds.panguh5.com:1998/' + _port ;
+_wport = '1997' ;
